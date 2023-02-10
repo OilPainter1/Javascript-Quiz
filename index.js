6d6e36cf96421682187b8c19999a3fea5a739f2c
OilPainter1 <hjcryns@gmail.com>
2023-02-09 18:30:32 -0600

    implemented button logic

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ startBtn.addEventListener("click",(event)=>{
     event.preventDefault
     startBtn.remove()
     renderTimer()
-    renderQuestions(0)
-    setTimeout(timeRanOut,6000)
+    renderQuestions(0,0)
+    setTimeout(timeRanOut,20000)
   
 })
 
 
+function increaseNumCorrect() {
+    var numCorrect = document.getElementById("correct")
+    numCorrect.textContent++
+}
 
 
 function renderTimer(){
     var timer = document.createElement("div")
+    timer.setAttribute("id","time")
     var header = document.getElementById("header")
     header.appendChild(timer)
-    timer.textContent=6
+    timer.textContent=20
     setInterval(function(){
         if(timer.textContent==0){
             return
@@ -31,22 +36,44 @@ function timeRanOut(){
     console.log("out of time")
 }
 
-function renderQuestions(index){
-    document.getElementById('question').textContent=questions[index]
-    document.getElementById('btn1').textContent= answers[index][0]
-    document.getElementById('btn2').textContent= answers[index][1]
-    document.getElementById('btn3').textContent= answers[index][2]
-    document.getElementById('btn4').textContent= answers[index][3]
-    document.getElementById('btn1').addEventListener("click",(event)=>{
-        document.getElementById("correct").textContent=document.getElementById("correct").textContent++
-    })    
+function renderQuestions(index,correctAnswer){
+    var timer = document.getElementById("time")
+    var question = document.createElement("h2")
+    var section = document.getElementById("questionSection")
+    section.appendChild(question)
+    question.setAttribute("id", "question")
+    question.textContent=questions[index]
+    question.addEventListener("click", (event)=>{
+        event.preventDefault
+        event.stopPropagation
+        if(event.target.id == correctAnswer){
+            increaseNumCorrect()
+            section.removeChild(question)
+            renderQuestions(++index,2)
+            return
+        }else{
+            section.removeChild(question)
+            timer.textContent = timer.textContent - 5
+
+        }
+        
+    })
+    renderButtons(index)   
     return
 }
 
+function renderButtons(index){
+    for (var i =0; i < 4; i++){
+        var button = document.createElement('button')
+        button.textContent= answers[index][i]
+        button.setAttribute("id", i)
+        document.getElementById("question").appendChild(button)
+    }
+}
 
 
 
 
 var questions=["What does DOM stand for?", "Which of the following is not a javascript type?", "Which of the following is javascript for the logical 'AND' operator?", "Which of the following is a boolean?","Which of the following does not evaluate to true?"]
 
-var answers=[["Document Object Model","Document Object Manuscript","Deletable Object Manipulation","Document Object Manipulation"]]
+var answers=[["Document Object Model","Document Object Manuscript","Deletable Object Manipulation","Document Object Manipulation"],["test1","test2","test3","test4"]]
